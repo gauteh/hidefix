b678e99c208a5a3245086d18ec87c08994164db6
Gaute Hope <eg@gaute.vetsj.com>
2020-02-10 19:37:23 +0100

    add uring feature flag

--- benches/read_performance.rs
+++ benches/read_performance.rs
@@ -2,7 +2,7 @@
 extern crate test;
 use test::Bencher;
 
-use hidefix::{idx::Index, reader::DatasetReader, uring};
+use hidefix::{idx::Index, reader::DatasetReader};
 
 #[bench]
 fn read_2d_chunked_idx(b: &mut Bencher) {
@@ -29,14 +29,6 @@ fn read_t_float32_idx(b: &mut Bencher) {
 }
 
 #[bench]
-fn read_t_float32_idx_rio(b: &mut Bencher) {
-    let i = Index::index("tests/data/t_float.h5").unwrap();
-    let mut r = uring::DatasetReader::with_dataset(i.dataset("d32_1").unwrap(), i.path()).unwrap();
-
-    b.iter(|| r.values::<f32>(None, None).unwrap())
-}
-
-#[bench]
 fn read_t_float32_nat(b: &mut Bencher) {
     let h = hdf5::File::open("tests/data/t_float.h5").unwrap();
     let d = h.dataset("d32_1").unwrap();
@@ -58,4 +50,17 @@ fn read_chunked_1d_nat(b: &mut Bencher) {
     let d = h.dataset("d_4_chunks").unwrap();
 
     b.iter(|| d.read_raw::<f32>().unwrap())
+}
+
+#[cfg(feature = "io_uring")]
+mod uring {
+    use hidefix::uring;
+
+    #[bench]
+    fn read_t_float32_idx_rio(b: &mut Bencher) {
+        let i = Index::index("tests/data/t_float.h5").unwrap();
+        let mut r = uring::DatasetReader::with_dataset(i.dataset("d32_1").unwrap(), i.path()).unwrap();
+
+        b.iter(|| r.values::<f32>(None, None).unwrap())
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,8 @@ extern crate anyhow;
 
 pub mod idx;
 pub mod reader;
+
+#[cfg(feature = "io_uring")]
 pub mod uring;
 
 #[cfg(test)]
