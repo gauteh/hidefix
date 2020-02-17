a7deca2427dc99378a86016069554e4841d7f991
Gaute Hope <eg@gaute.vetsj.com>
2020-02-17 13:29:54 +0100

    bench: slicer benchmark

--- benches/index_performance.rs
+++ benches/index_performance.rs
@@ -8,3 +8,16 @@ use hidefix::idx::Index;
 fn read_2d_chunked_idx(b: &mut Bencher) {
     b.iter(|| Index::index("tests/data/chunked_oneD.h5").unwrap())
 }
+
+mod coads {
+    use super::*;
+
+    #[bench]
+    fn slicer(b: &mut Bencher) {
+        let i = Index::index("../data/coads_climatology.nc4").unwrap();
+        let d = i.dataset("SST").unwrap();
+
+        b.iter(|| d.chunk_slices(None, None).for_each(drop))
+    }
+}
+
