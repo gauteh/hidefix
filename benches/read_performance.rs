174c476dce12c5473cdde94652e098d01fe2f4fd
Gaute Hope <eg@gaute.vetsj.com>
2020-02-18 20:44:54 +0100

    bench: compare shuf / comp

--- benches/read_performance.rs
+++ benches/read_performance.rs
@@ -38,12 +38,28 @@ fn read_2d_shuffled_cache(b: &mut Bencher) {
 }
 
 #[bench]
+fn read_2d_shuffled_cache_nat(b: &mut Bencher) {
+    let h = hdf5::File::open("tests/data/dmrpp/chunked_shuffled_twoD.h5").unwrap();
+    let d = h.dataset("d_4_shuffled_chunks").unwrap();
+
+    b.iter(|| d.read_raw::<f32>().unwrap())
+}
+
+#[bench]
 fn read_2d_compressed_cache(b: &mut Bencher) {
     let i = Index::index("tests/data/dmrpp/chunked_gzipped_twoD.h5").unwrap();
     let mut r =
         cache::DatasetReader::with_dataset(i.dataset("d_4_gzipped_chunks").unwrap(), i.path()).unwrap();
 
     b.iter(|| r.values::<f32>(None, None).unwrap())
+}
+
+#[bench]
+fn read_2d_compressed_cache_nat(b: &mut Bencher) {
+    let h = hdf5::File::open("tests/data/dmrpp/chunked_gzipped_twoD.h5").unwrap();
+    let d = h.dataset("d_4_gzipped_chunks").unwrap();
+
+    b.iter(|| d.read_raw::<f32>().unwrap())
 }
 
 #[bench]
@@ -54,6 +70,15 @@ fn read_2d_shuffled_compressed_cache(b: &mut Bencher) {
 
     b.iter(|| r.values::<f32>(None, None).unwrap())
 }
+
+#[bench]
+fn read_2d_shuffled_compressed_cache_nat(b: &mut Bencher) {
+    let h = hdf5::File::open("tests/data/dmrpp/chunked_shufzip_twoD.h5").unwrap();
+    let d = h.dataset("d_4_shufzip_chunks").unwrap();
+
+    b.iter(|| d.read_raw::<f32>().unwrap())
+}
+
 
 #[bench]
 fn read_2d_chunked_idx_stream(b: &mut Bencher) {
