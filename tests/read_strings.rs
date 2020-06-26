a18e07b23e9271b73b6c1bca38a7e7bb860b9493
Gaute Hope <eg@gaute.vetsj.com>
2020-06-26 09:07:00 +0200

    test: read string array

---
+++ tests/read_strings.rs
@@ -0,0 +1,19 @@
+#![feature(test)]
+extern crate test;
+
+use hidefix::idx::Index;
+use ndarray::s;
+
+#[test]
+fn chunked_string_array() {
+    type T = u8;
+
+    let i = Index::index("tests/data/dmrpp/chunked_string_array.h5").unwrap();
+    let mut r = i.reader("string_array").unwrap();
+
+    let values = r.values::<T>(None, None).unwrap();
+    let strs = std::str::from_utf8(&values).unwrap();
+    println!("{:?}", strs);
+
+    assert_eq!(strs, "wqwqt\u{0}\u{0}\u{0}jhgjhgjhkjhkjhk\u{0}ddsfdsg\u{0}njiuh\u{0}\u{0}\u{0}");
+}
