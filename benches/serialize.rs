34556e9ecb304bac5b787d3935898dfa6f40e011
Gaute Hope <eg@gaute.vetsj.com>
2022-05-03 14:21:42 +0200

    set up for benching index in different dbs

--- benches/serialize.rs
+++ benches/serialize.rs
@@ -89,47 +89,6 @@ mod serde_bincode {
 
     #[ignore]
     #[bench]
-    fn deserialize_meps_bincode_db_sled(b: &mut Bencher) {
-        let i = Index::index("tests/data/meps_det_vc_2_5km_latest.nc").unwrap();
-
-        let bts = bincode::serialize(&i).unwrap();
-
-        let db = sled::Config::default()
-            .temporary(true)
-            .print_profile_on_drop(true)
-            .open()
-            .unwrap();
-
-        db.insert("meps", bts).unwrap();
-
-        b.iter(|| {
-            let bts = db.get("meps").unwrap().unwrap();
-            test::black_box(bincode::deserialize::<Index>(&bts).unwrap());
-        })
-    }
-
-    #[ignore]
-    #[bench]
-    fn deserialize_meps_bincode_db_sled_only_read(b: &mut Bencher) {
-        let i = Index::index("tests/data/meps_det_vc_2_5km_latest.nc").unwrap();
-
-        let bts = bincode::serialize(&i).unwrap();
-
-        let db = sled::Config::default()
-            .temporary(true)
-            .print_profile_on_drop(true)
-            .open()
-            .unwrap();
-
-        db.insert("meps", bts).unwrap();
-
-        b.iter(|| {
-            test::black_box(db.get("meps").unwrap().unwrap());
-        })
-    }
-
-    #[ignore]
-    #[bench]
     fn deserialize_meps_file_only_read(b: &mut Bencher) {
         let i = Index::index("tests/data/meps_det_vc_2_5km_latest.nc").unwrap();
         let f = std::fs::File::create("/tmp/meps.idx.bc").unwrap();
