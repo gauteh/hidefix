ff9b2d9fd882b329aac7d42aae4c586c03e34fdc
Gaute Hope <eg@gaute.vetsj.com>
2022-12-24 11:48:12 +0100

    uring: reading sst

--- src/reader/uring.rs
+++ src/reader/uring.rs
@@ -1,27 +1,28 @@
-use std::path::Path;
+use std::io::{Read, Seek};
 
 // use super::{chunk::read_chunk, dataset::Reader};
 use super::{chunk::read_chunk, dataset::Reader};
 use crate::filters::byteorder::Order;
 use crate::idx::{Chunk, Dataset};
 
-pub struct UringReader<'a, const D: usize> {
+pub struct UringReader<'a, R: Read + Seek, const D: usize> {
     ds: &'a Dataset<'a, D>,
+    fd: R,
     chunk_sz: u64,
 }
 
-impl<'a, const D: usize> UringReader<'a, D> {
-    pub fn with_dataset<P: AsRef<Path>>(
+impl<'a, R: Read + Seek, const D: usize> UringReader<'a, R, D> {
+    pub fn with_dataset(
         ds: &'a Dataset<D>,
-        path: P,
-    ) -> Result<UringReader<'a, D>, anyhow::Error> {
+        fd: R,
+    ) -> Result<UringReader<'a, R, D>, anyhow::Error> {
         let chunk_sz = ds.chunk_shape.iter().product::<u64>() * ds.dsize as u64;
 
-        Ok(UringReader { ds, chunk_sz })
+        Ok(UringReader { ds, fd, chunk_sz })
     }
 }
 
-impl<'a, const D: usize> Reader for UringReader<'a, D> {
+impl<'a, R: Read + Seek, const D: usize> Reader for UringReader<'a, R, D> {
     fn order(&self) -> Order {
         self.ds.order
     }
@@ -38,7 +39,7 @@ impl<'a, const D: usize> Reader for UringReader<'a, D> {
         &mut self,
         indices: Option<&[u64]>,
         counts: Option<&[u64]>,
-        mut dst: &mut [u8],
+        dst: &mut [u8],
     ) -> Result<usize, anyhow::Error> {
         let indices: Option<&[u64; D]> = indices
             .map(|i| i.try_into())
@@ -73,7 +74,7 @@ impl<'a, const D: usize> Reader for UringReader<'a, D> {
             .collect::<Vec<_>>();
 
         // Sort chunk file address, not destination address.
-        chunks.sort_unstable_by_key(|(current, c, start, end)| c.addr.get());
+        chunks.sort_unstable_by_key(|(_current, c, _start, _end)| c.addr.get());
 
         // Group by chunk
         let mut groups = Vec::<(&Chunk<D>, Vec<(u64, u64, u64)>)>::new();
@@ -103,9 +104,9 @@ impl<'a, const D: usize> Reader for UringReader<'a, D> {
             )?;
 
             for (current, start, end) in segments {
-                let start = start as usize;
-                let end = end as usize;
-                let current = current as usize;
+                let start = (start * dsz) as usize;
+                let end = (end * dsz) as usize;
+                let current = (current * dsz) as usize;
 
                 debug_assert!(start <= cache.len());
                 debug_assert!(end <= cache.len());
@@ -116,5 +117,31 @@ impl<'a, const D: usize> Reader for UringReader<'a, D> {
         }
 
         Ok(vsz as usize)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::prelude::*;
+    use super::*;
+    use std::fs;
+    use crate::idx::DatasetD;
+
+    #[test]
+    fn read_coads_sst() {
+        let i = Index::index("tests/data/coads_climatology.nc4").unwrap();
+        let ds = if let DatasetD::D3(ds) = i.dataset("SST").unwrap() {
+            ds
+        } else {
+            panic!()
+        };
+        let mut r = UringReader::with_dataset(ds, fs::File::open(i.path().unwrap()).unwrap()).unwrap();
+
+        let vs = r.values::<f32>(None, None).unwrap();
+
+        let h = hdf5::File::open(i.path().unwrap()).unwrap();
+        let hvs = h.dataset("SST").unwrap().read_raw::<f32>().unwrap();
+
+        assert_eq!(vs, hvs);
     }
 }
