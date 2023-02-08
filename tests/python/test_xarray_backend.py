47559b93885a9bfe315f6d3bb50c4c1f625569a6
Gaute Hope <eg@gaute.vetsj.com>
2023-02-08 14:12:30 +0100

    xarray: test mfdataset

--- tests/python/test_xarray_backend.py
+++ tests/python/test_xarray_backend.py
@@ -3,6 +3,7 @@ from hidefix import xarray
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def test_coads_hf(coads, plot):
     ds = xr.open_dataset(coads, engine='hidefix', decode_times=False)
     print(ds)
@@ -11,7 +12,6 @@ def test_coads_hf(coads, plot):
     print(sst)
     print(sst.shape)
     print(sst.values.shape)
-
 
     if plot:
         sst.plot()
@@ -28,3 +28,18 @@ def test_coads_nc(coads, plot):
     if plot:
         ds['SST'].plot()
         plt.show()
+
+
+def test_xarray_mfdataset(data):
+    urls = [str(data / 'jan.nc4'), str(data / 'feb.nc4')]
+    ds = xr.decode_cf(xr.open_mfdataset(urls, engine='hidefix'))
+    print(ds)
+
+
+def test_xarray_mfdataset_nora3(data):
+    urls = [
+        '/lustre/storeB/project/fou/om/NORA3/equinor/atm_hourly/arome3km_1hr_198501.nc',
+        '/lustre/storeB/project/fou/om/NORA3/equinor/atm_hourly/arome3km_1hr_198502.nc'
+    ]
+    ds = xr.open_mfdataset(urls, engine='hidefix')
+    print(ds)
